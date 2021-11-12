87d0f037717843739b999967d7b125fbc4a1b8c8
Ted Wang <ttwang@ttwang-PC.lan>
2021-11-11 23:36:40 -0800

    Reduced range at which bots in hl2dm will use shotgun.

--- bot/mods/hl2dm/weapon/ShotgunFunction.h
+++ bot/mods/hl2dm/weapon/ShotgunFunction.h
@@ -5,7 +5,7 @@
 class ShotgunFunction: public WeaponFunction {
 public:
 	ShotgunFunction(): WeaponFunction(0.96f) {
-		range[1] = 768.0f;
+		range[1] = 400.0f;
 	}
 
 	void attack(Buttons& buttons, float distance) const;
