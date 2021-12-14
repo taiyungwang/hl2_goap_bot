6e522246164b695de4d063f4ac3bec94dce9c0e4
Ted Wang <ttwang@ttwang-PC.lan>
2021-12-14 10:23:50 -0800

    Increased the minimum distance at which DOD MG will deploy MG.

--- bot/mods/dod/weapon/DODMGBuilder.h
+++ bot/mods/dod/weapon/DODMGBuilder.h
@@ -8,7 +8,7 @@ class DODMGBuilder: public DeployableWeaponBuilder<DODMGReloader, DODMGDeployer>
 public:
 	DODMGBuilder() :
 			DeployableWeaponBuilder(0.8f, 0.0f, 3600.0f, "CDODBipodWeapon",
-					"m_bDeployed", 200.0f) {
+					"m_bDeployed", 1000.0f) {
 	}
 
 	std::shared_ptr<Weapon> build(edict_t* weap) const;
