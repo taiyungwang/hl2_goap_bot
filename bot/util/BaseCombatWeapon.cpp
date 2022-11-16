616ead55bb53cba5061f0c7a78a88f06fca22e1f
Ted Wang <ttwang@ttwang-PC.lan>
2022-11-16 11:22:06 -0800

    Added missing files from last few commits.

---
+++ bot/util/BaseCombatWeapon.cpp
@@ -0,0 +1,6 @@
+#include "../../hl2_goap_bot/bot/util/BaseCombatWeapon.h"
+
+const char* BaseCombatWeapon::CLIP_TYPES[2] = { "m_iClip1", "m_iClip2" };
+
+const char* BaseCombatWeapon::AMMO_TYPES[2] = { "m_iPrimaryAmmoType",
+		"m_iSecondaryAmmoType" };
